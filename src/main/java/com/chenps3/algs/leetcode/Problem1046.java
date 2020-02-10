71d3b2748f02194a8809b7f39fd18d1eb7bdf2bf
chenguanhong1792 <chenguanhong1792@tops001.com>
2020-02-10 13:02:20 +0800

    stage

---
+++ src/main/java/com/chenps3/algs/leetcode/Problem1046.java
@@ -0,0 +1,112 @@
+package com.chenps3.algs.leetcode;
+
+/**
+ * https://leetcode.com/problems/last-stone-weight/
+ * Heap
+ *
+ * @Author chenguanhong
+ * @Date 2020-02-08
+ */
+public class Problem1046 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int lastStoneWeight(int[] stones) {
+        IntMaxHeap heap = new IntMaxHeap(stones.length);
+        for (int i = 0; i < stones.length; i++) {
+            heap.insert(stones[i]);
+        }
+        while (heap.size() > 1) {
+            int y = heap.delMax();
+            int x = heap.delMax();
+            if (y > x) {
+                heap.insert(y - x);
+            }
+        }
+        if (heap.isEmpty()) {
+            return 0;
+        } else {
+            return heap.delMax();
+        }
+    }
+
+    static class IntMaxHeap {
+
+        public IntMaxHeap(int capacity) {
+            pq = new int[capacity + 1];
+            n = 0;
+        }
+
+        public void insert(int val) {
+            if (n + 1 == pq.length) {
+                resize(pq.length * 2);
+            }
+            pq[++n] = val;
+            swim(n);
+        }
+
+        public int delMax() {
+            if (isEmpty()) {
+                throw new RuntimeException("empty heap");
+            }
+            int max = pq[1];
+            exch(1, n--);
+            sink(1);
+            return max;
+        }
+
+        public int size() {
+            return n;
+        }
+
+        private int[] pq;
+
+        private int n;
+
+        private boolean less(int i, int j) {
+            return pq[i] < pq[j];
+        }
+
+        private void exch(int i, int j) {
+            int tmp = pq[i];
+            pq[i] = pq[j];
+            pq[j] = tmp;
+        }
+
+        private void swim(int k) {
+            while (k > 1 && less(k / 2, k)) {
+                exch(k / 2, k);
+                k = k / 2;
+            }
+        }
+
+        private void sink(int k) {
+            while (2 * k <= n) {
+                int j = 2 * k;
+                //跟更大的子节点交换
+                if (j < n && less(j, j + 1)) {
+                    j++;
+                }
+                if (less(j, k)) {
+                    break;
+                }
+                exch(j, k);
+                k = j;
+            }
+        }
+
+        private boolean isEmpty() {
+            return n == 0;
+        }
+
+        private void resize(int capacity) {
+            int[] tmp = new int[capacity];
+            for (int i = 1; i <= n; i++) {
+                tmp[i] = pq[i];
+            }
+            pq = tmp;
+        }
+    }
+}
---
+++ src/main/java/com/chenps3/algs/leetcode/Problem215.java
@@ -0,0 +1,56 @@
+package com.chenps3.algs.leetcode;
+
+
+import edu.princeton.cs.algs4.StdIn;
+
+/**
+ * https://leetcode.com/problems/kth-largest-element-in-an-array/
+ * Heap
+ *
+ * @Author chenguanhong
+ * @Date 2020-02-09
+ */
+public class Problem215 {
+
+    public int findKthLargest(int[] nums, int k) {
+        heapSort(nums);
+        return nums[k - 1];
+    }
+
+    //倒序排序
+    private void heapSort(int[] nums) {
+        //1 构建最小堆
+        int n = nums.length;
+        for (int k = n / 2; k >= 1; k--) {
+            sink(nums, k, n);
+        }
+        while (n > 0) {
+            exch(nums, 1, n--);
+            sink(nums, 1, n);
+        }
+    }
+
+    private void sink(int[] nums, int k, int n) {
+        while (k * 2 <= n) {
+            int j = k * 2;
+            if (j < n && greater(nums, j, j + 1)) {
+                j++;
+            }
+            if (!greater(nums, k, j)) {
+                break;
+            }
+            exch(nums, j, k);
+            k = j;
+        }
+    }
+
+    private boolean greater(int[] nums, int i, int j) {
+        return nums[i - 1] > nums[j - 1];
+    }
+
+    private void exch(int[] nums, int i, int j) {
+        int tmp = nums[i - 1];
+        nums[i - 1] = nums[j - 1];
+        nums[j - 1] = tmp;
+    }
+}
