154e8c7de1f143dce30da20da0a414e4618fdaed
chenguanhong1792 <chenguanhong1792@tops001.com>
2020-03-07 18:00:21 +0800

    stage

---
+++ algs-leetcode/src/main/java/com/chenps3/algs/leetcode/Offer57I.java
@@ -0,0 +1,56 @@
+package com.chenps3.algs.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode-cn.com/problems/he-wei-sde-lian-xu-zheng-shu-xu-lie-lcof/
+ * 剑指offer
+ *
+ * @Author chenguanhong
+ * @Date 2020-03-06
+ */
+public class Offer57I {
+
+    public static void main(String[] args) {
+        int[][] a1 = findContinuousSequence(15);
+        System.out.println();
+    }
+
+    public static int[][] findContinuousSequence(int target) {
+        int hi = target % 2 == 0 ? target / 2 : target / 2 + 1; //最大值在一半左右
+        int left = 1;
+        int right = 2;
+        List<int[]> list = new ArrayList<>();
+        while (left < right && right <= hi) {
+            int sum = sum(left, right);       //等差数列求和
+            if (sum < target) {
+                right++;
+            } else if (sum > target) {
+                left++;
+            } else if (sum == target) {
+                list.add(generateArr(left, right));
+                left++;
+            }
+        }
+        int[][] res = new int[list.size()][];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+
+    private static int sum(int lo, int hi) {
+        return (lo + hi) * (hi - lo + 1) / 2;
+    }
+
+    private static int[] generateArr(int lo, int hi) {
+        int[] arr = new int[hi - lo + 1];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = lo++;
+        }
+        return arr;
+    }
+
+
+}
---
+++ algs-leetcode/src/main/java/com/chenps3/algs/leetcode/Offer59II.java
@@ -0,0 +1,72 @@
+package com.chenps3.algs.leetcode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * https://leetcode-cn.com/problems/dui-lie-de-zui-da-zhi-lcof/
+ *
+ * @Author chenguanhong
+ * @Date 2020-03-07
+ */
+public class Offer59II {
+
+    public static void main(String[] args) {
+        Offer59II m = new Offer59II();
+        m.push_back(1);
+        m.push_back(2);
+        System.out.println(m.max_value());
+        System.out.println(m.pop_front());
+        System.out.println(m.pop_front());
+    }
+
+    public Offer59II() {
+        currIndex = 0;
+        data = new LinkedList<>();
+        max = new LinkedList<>();
+    }
+
+    public int max_value() {
+        if (max.isEmpty()) {
+            return -1;
+        }
+        return max.getFirst().val;
+    }
+
+    public void push_back(int value) {
+        //后进的元素A如果比前面的元素都大，A之前的元素无论怎么pop，max都是A
+        while (!max.isEmpty() && value >= max.getLast().val) {
+            max.removeLast();
+        }
+        InternalData i = new InternalData(currIndex, value);
+        data.addLast(i);
+        max.addLast(i);
+        currIndex++;
+    }
+
+    public int pop_front() {
+        if (max.isEmpty()) {
+            return -1;
+        }
+        //说明max被pop出去了
+        if (max.getFirst().index == data.getFirst().index) {
+            max.removeFirst();
+        }
+        return data.removeFirst().val;
+    }
+
+    private static class InternalData {
+        private int index;
+        private int val;
+
+        public InternalData(int index, int val) {
+            this.index = index;
+            this.val = val;
+        }
+    }
+
+    private LinkedList<InternalData> data;
+    private LinkedList<InternalData> max;
+    private int currIndex;
+
+}
